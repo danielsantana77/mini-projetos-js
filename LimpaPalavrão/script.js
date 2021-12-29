722c872b01c04f8c0f9aa569d6b295730653ad79
danielsantana77 <daniel_santana_oliveira@hotmail.com>
2021-12-29 13:54:28 -0300

    Projeto completo

---
+++ LimpaPalavrão/script.js
@@ -0,0 +1,74 @@
+function limparTexto(){
+
+    const btnLimpar = document.getElementById('btn-Limpar')
+ 
+    if(btnLimpar.innerText == "Limpar outra frase"){
+        location.reload(true)
+        return
+    }
+
+    const regex = /\bporra\b|caraio|merda|poha|puta/gi  //Lista de palavrões 
+    let contadorDePalavroes = 0
+
+    const texto = document.getElementById('campoTexto').value
+    const campoQtdPalavroes = document.getElementById('quantidadePalavroes')
+ 
+    var arrayDePalavras = texto.split(" ")
+    
+    arrayDePalavras = arrayDePalavras.filter(function(element) {
+
+        if(element != " " && element != "\n"){
+            return element
+        }
+    }) 
+
+    
+    arrayDePalavras.forEach((element, index) => {
+        
+        if(element.match(regex)){
+           arrayDePalavras[index]=element.replace(regex, "***")
+            contadorDePalavroes++
+        } 
+
+        
+
+    });
+
+    
+  
+ 
+
+
+    if(texto == ""){
+        alert("Coloque a sua frase no campo !")
+    }else if(contadorDePalavroes == 0){
+        alert("Sua frase está limpinha !")
+    }else{
+
+        const campoTexto = document.getElementById('campoTexto')
+        campoTexto.value = arrayDePalavras.join(" ")
+        campoTexto.style.border = "1px solid #00d48f"
+        
+    
+        campoQtdPalavroes.innerHTML = "<strong>" + contadorDePalavroes  + " palavrões " + "</strong>" + "foram removidos :)"
+    
+        const fraseLimpa = document.getElementById('fraseLimpa')
+     
+        fraseLimpa.toggleAttribute('hidden')
+
+        
+        btnLimpar.style.backgroundColor = "#00d48f"
+        btnLimpar.style.color = "#fff"
+        btnLimpar.innerText = "Limpar outra frase"
+
+        
+    }
+
+    
+    
+}
+
+function contarCaracter(valor){
+    var cont = valor.length; 
+    document.getElementById("quantidade").innerHTML = cont ;
+}
